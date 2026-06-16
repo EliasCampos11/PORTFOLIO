37c7a7520ef5dab1ef2ff9ddfbb7310ea25038dd
Matheus Daniel de Almeida Silva <125888985+MatheusDaniel009@users.noreply.github.com>
2026-06-16 12:14:25 -0300

    refatorando com generics viewrs e criando uma fk para user

--- estoque/models.py
+++ estoque/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Produto(models.Model):
+
+    vendedor = models.ForeignKey('auth.User', related_name="produtos", on_delete=models.CASCADE )
     nome = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
     quantidade_estoque = models.IntegerField()
--- estoque/views.py
+++ estoque/views.py
@@ -9,40 +9,20 @@ from rest_framework import mixins, generics
 from estoque.models import Produto
 from estoque.serializers import ProdutoSerializer
 
-class ProdutoDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class ProdutoDetail( generics.RetrieveUpdateDestroyAPIView):
     queryset= Produto.objects.all()
     serializer_class = ProdutoSerializer
     lookup_field = "id"
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-
     def perform_destroy(self, instance):
         instance.disponivel_produto = False
         instance.save()
 
-    def delete(self, request,*args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-class ProdutosList(
-mixins.ListModelMixin, #adicionar mixin de listagem
-mixins.CreateModelMixin, #adicionar mixin de criacao
-generics.GenericAPIView, #classe generica
-):
-    queryset= Produto.objects.filter(disponivel_produto=True)
-    serializer_class = ProdutoSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
+class ProdutosList( generics.ListCreateAPIView):
     
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    serializer_class = ProdutoSerializer
+
+    def get_queryset(self):
+        username = self.request.query_params.get("username", None)
+        queryset= Produto.objects.filter(vendedor__username=username, disponivel_produto=True)
+        return queryset
+
